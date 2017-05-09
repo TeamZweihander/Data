9617c0818a315f238750a309d197255a47850ff4
XolieSwa <x.ntshingila@gmail.com>
2017-05-09 18:59:37 +0200

    Initial project file

---
+++ Demo 2/DataStream/src/DataStream.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author xoliswa.ntshingila,diana.obo,martha.mohlala ,mark.klingenberg
+ */
+public class DataStream {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
